bb1774a408d9206ed0f0092f065971b6d0dd34d8
Sachin Gupta <sachin.gupta@peason.com>
2026-04-11 20:00:13 +0530

    Wrangler Changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // Enables static HTML export
+  trailingSlash: true,
+  images: {
+    unoptimized: true, // Required for static export
+  },
 };
 
 export default nextConfig;
